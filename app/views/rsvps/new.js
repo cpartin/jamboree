964f5b9702589b13e25ff70b04c54f6170607c01
Craig Partin <cpartin@optoro.com>
2015-08-28 23:04:13 -0400

    site updates

--- app/views/rsvps/new.js
+++ app/views/rsvps/new.js
@@ -1,3 +1,6 @@
 window.scrollTo(0,0);
 $("#form").show();
 $("#form").html("<%= escape_javascript(render partial: 'new', locals: { rsvp: @rsvp } ) %>");
+$('#cancel').click(function() {
+  $("#form").hide();
+});
